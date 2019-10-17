ddc57d2ecdc30997ca4f08bc989bb206023b6060
Alexandru Damian <alexdamian121@gmail.com>
2019-10-17 09:43:12 +0300

    Fixed naming in main file

--- main.cpp
+++ main.cpp
@@ -1,20 +1,17 @@
-#include "ReferenceCountedPointer.h"
-
-#include "pch.h"
+#include "RefCounter.h"
 #include <iostream>
 
 
 int main() 
 {
-	smart_ptr::RefCount refCount;
-	std::cout << "current count:" << refCount.get_count() << std::endl;
+	smart_ptr::RefCounter refCounter;
+	std::cout << "current count:" << refCounter.get_count() << std::endl;
 
-	refCount.addRef();
-	std::cout<<"current count:"<<refCount.get_count()<<std::endl;
+	refCounter.addRef();
+	std::cout<<"current count:"<<refCounter.get_count()<<std::endl;
 
-	refCount.addRef();
-	std::cout << "current count:" << refCount.get_count() << std::endl;
-
+	refCounter.removeRef();
+	std::cout << "current count:" << refCounter.get_count() << std::endl;
 
 	return 0;
 }
